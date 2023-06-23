f95a8b1dca552ddd54fcb8e313da7780c82ee97d
prachi <prachiinbox100@gmail.com>
2023-06-23 18:17:41 +0530

    rajesh

---
+++ resources/views/emails/booking_cancel_mail.blade.php
@@ -0,0 +1,46 @@
+<style type="text/css">
+    body {
+        margin: 0px !important;
+    }
+</style>
+<div style="width: 945px; height: 65; background-color: EEDFD0; margin: auto; text-align: center; line-height: 60px;">
+    <h1>THNAK YOU COME AGAIN</h1>
+</div>    
+<div style="width: 945px; height: 65; margin: auto; padding: 0px 25px;">
+    <div style="width : 758px; margin : auto;">
+        <h1><b>We’re sad to see you go...</b></h1>
+        <p>Hi Hoe Wei,</p>
+        <p>We’ve received your request for cancellation of booking.</p>
+        <p>Your booking cancellation details are as follows:</p>
+    </div>
+    <div style="width: 758px; background-color: EEDFD0; margin : auto;">
+        <div style="padding: 20px 0px 40px 0px">
+            <h1 style="text-align : center; line-height: 0px;"><b><strong>Booking Cancellation</strong></b></h1>
+            <hr>
+            <div style="width: 518px; margin: 35px auto;">
+                <p>Name: <b>Hoe Wei</b></p>
+                <p>Email: <b>weige420@gmail.com</b></p>
+                <p>Outlet: <b>Thank You Come Again @ Balestier</b></p>
+                <p>Booking Space: <b>Hot Desk</b></p>
+                <p>Plan Type: <b>Day Plan</b></p>
+                <p>Appointment Date: <b>17 Mar 2023</b></p>
+                <p>Time Slot: <b>3.00 pm</b></p>
+                <p>Payable Amount: <b>$4.00</b></p>
+                <p>Payment Method: <b>PayPal</b></p>
+                
+            </div>
+            <a href="" style="text-decoration-line: none;"><p style="width: 298px; height: 42px; margin: auto;text-align: center; background-color: #000000; line-height: 40px; color: #ffffff;">View Booking</p></a>
+        </div>
+    </div>
+    <div style="width: 758px; margin : auto;">
+        <p style="padding-bottom: 34px; text-align: center; line-height: 30px;">We hope to catch you next time, at TYCA!</p>
+    </div>
+</div>
+
+
+
+
+
+ 
+
+
